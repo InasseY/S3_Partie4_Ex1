f6379b3f473fef793f8ded923834e04c0c002adb
InasseY <inasse.youssouf@gmail.com>
2021-02-18 14:27:12 +0100

    creation of the array and add & correct a new value

---
+++ Partie4_Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Partie4_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creation of the array
+            string[] week = new string[] {"lundi", "mardi", "mercredi", "jedi","vendredi", "samedi" };
+
+            Console.WriteLine($"l'indice numéro 1 est { week[1]},\net l'indice numéro 4 est { week[4]}");
+            Console.WriteLine($"le contenu du 1er élement est { week[0]},\net le contenu du 4ème élément est { week[3]}");
+
+            //changer une donnée en la redéfinissant
+            week[3] = "jeudi";
+            Console.WriteLine($"la modification a été faite, à présent c'est { week[3]} ");
+
+            //fonction ajout "arrayresize" + référence du tableau + taille 
+            Array.Resize(ref week, 7);
+            week[6] = "dimanche";
+            Console.WriteLine($"un dernier jour est ajouté { week[6]} ");
+
+            //boucle pour afficher tout le tableau 
+            for (int i = 0; i < week.Length; i++)
+            Console.WriteLine(week[i]);
+        }
+        }
+    }
+
